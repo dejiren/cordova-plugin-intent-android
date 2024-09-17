6bb45da539a48d2d6a9917d1f865ebff442c3e5b
ossun.l <ossun.l@wingarc.com>
2024-09-17 13:36:47 +0900

    update import

--- src/android/IntentPlugin.java
+++ src/android/IntentPlugin.java
@@ -13,6 +13,7 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
